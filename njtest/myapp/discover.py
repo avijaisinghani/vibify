1b4a75e084888f9c22a47ef763d146653a61dcba
Avinash Jaisinghani <avi@Avinashs-MacBook-Pro.local>
2016-07-26 17:56:25 -0400

    removing delete query

--- njtest/myapp/discover.py
+++ njtest/myapp/discover.py
@@ -100,9 +100,6 @@ def post_playlist(environ):
   location_json = {
     'location': newlistredirect
   }
-  user = args['user'][0]
-  cursor.execute ('delete from tracks where user = ?',(user,))
-  cursor.execute ('delete from playlists where user = ?',(user,))
   print(newlistredirectbyte)
   return (newlistredirectbyte)
   con.commit()
